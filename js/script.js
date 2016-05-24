979dc53b82cf74e6af2aa37cac3bdb4f71d9a1a9
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-05-24 22:29:34 +0800

    add hover tint effect

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	for (var i = 0; i < 16; i++)
-		$('#gameboard').append("<div></div>");
+		$("#gameboard").append("<div></div>");
 	for (var i = 0; i < 16; i++)
-		$('#gameboard > div').append("<div>0</div>")
+		$("#gameboard > div").append("<div></div>")
+	$("#gameboard div div").hover(function(){
+		$(this).css("background", "#666666");
+	});
 });
